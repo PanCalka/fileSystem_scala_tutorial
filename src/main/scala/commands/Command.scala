e91d8368314407c32a61ea513605b0299a1a4961
Rafal Wojcik <rafal.wojcik@synapse.com>
2018-11-08 17:18:05 +0100

    start to implementing mkdir

--- src/main/scala/commands/Command.scala
+++ src/main/scala/commands/Command.scala
@@ -4,12 +4,27 @@ import filesys.State
 
 trait Command {
 
+
   def apply(state: State):State
 
 
 }
 
 object Command {
+  val MKDIR = "mkdir"
 
-  def from(input: String): Command = new UnknownCommand
+  def emptyCommand : Command = (state: State) => state
+
+  def incompleteCommand(name: String) : Command = (state: State) => state.setMessage(name + " incomplete command")
+
+  def from(input: String): Command = {
+    val tokens: Array[String] = input.split(" ")
+
+    if(tokens.isEmpty) emptyCommand
+    else if (MKDIR.equals(tokens(0))) {
+      if (tokens.length < 2) incompleteCommand(MKDIR)
+      else new MkDir(tokens(1))
+    }
+    else new UnknownCommand
+  }
 }
---
+++ src/main/scala/commands/MkDir.scala
@@ -0,0 +1,6 @@
+package commands
+import filesys.State
+
+class MkDir(name: String) extends Command {
+  override def apply(state: State): State = ???
+}
