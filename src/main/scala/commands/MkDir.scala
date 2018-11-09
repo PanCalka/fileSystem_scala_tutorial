a2feae07aa3a1f019e91933ceb40da6993b294b9
Rafal Wojcik <rafal.wojcik@synapse.com>
2018-11-09 11:02:12 +0100

    adding mkdir apply method, and doMkdir todo

--- src/main/scala/commands/MkDir.scala
+++ src/main/scala/commands/MkDir.scala
@@ -1,6 +1,26 @@
 package commands
+import files.Directory
 import filesys.State
 
 class MkDir(name: String) extends Command {
-  override def apply(state: State): State = ???
+
+  def checkIllegal(name: String): Boolean = {
+    name.contains(".")
+  }
+
+  def doMkDir(state: State, name: String): State = ???
+
+  override def apply(state: State): State = {
+    val wd = state.wd
+    if(wd.hasEntry(name)){
+      state.setMessage("Entry " + name + " already exist")
+    } else if(name.contains(Directory.SEPARATOR)){
+      state.setMessage(name + "shouldnt contain separators")
+    } else if(checkIllegal(name)) {
+      state.setMessage(name + ": illegal entry name")
+    } else {
+      doMkDir(state, name)
+    }
+
+  }
 }
